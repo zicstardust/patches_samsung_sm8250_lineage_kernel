0cc142ee678134c82056ee6e8b25369c01190cba
Zic <git@zicstardust.com>
2026-06-04 18:18:06 -0400

    fix install script to use consistent temporary directory name

--- install.sh
+++ install.sh
@@ -3,30 +3,32 @@
 #fi
 
 
-if [ -d "/tmp/patchs-sm8250" ]; then
-    rm -rf /tmp/patchs-sm8250
+if [ -d "/tmp/zic-patchs-sm8250" ]; then
+    rm -rf /tmp/zic-patchs-sm8250
 fi
 
 current_dir=$(dirname "$(readlink -f "$0")")
 #current_dir=$(pwd)
 
-git clone --depth=1 https://github.com/zicstardust/patchs-sm8250.git /tmp/patchs-sm8250
+git clone --depth=1 https://github.com/zicstardust/patchs-sm8250.git /tmp/zic-patchs-sm8250 -b lineage-23.2
 
 cd kernel/samsung/sm8250
 
 curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s legacy
 
-git apply < /tmp/patchs-sm8250/patches/kernel_kernelsu.patch
-git apply < /tmp/patchs-sm8250/patches/kernel_droidspaces.patch
+git apply < /tmp/zic-patchs-sm8250/patches/kernel_kernelsu.patch
+git apply < /tmp/zic-patchs-sm8250/patches/kernel_droidspaces.patch
 
-cp /tmp/patchs-sm8250/configs/*.config arch/arm64/configs/
+cp /tmp/zic-patchs-sm8250/configs/*.config arch/arm64/configs/
 
 cd KernelSU-Next
 
-git apply < /tmp/patchs-sm8250/patches/kernelsu-next.patch
+git apply < /tmp/zic-patchs-sm8250/patches/kernelsu-next.patch
 
 cd "$current_dir/device/samsung/sm8250-common"
 
-git apply < /tmp/patchs-sm8250/patches/device.patch
+git apply < /tmp/zic-patchs-sm8250/patches/device.patch
+
+rm -rf /tmp/zic-patchs-sm8250
 
 cd "$current_dir"
